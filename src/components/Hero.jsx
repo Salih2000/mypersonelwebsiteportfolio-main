1240a263f6a1d41502175bf4dddb242b9643061c
Salih Abdullah Kilic <salihabdullahkilic@hotmail.com>
2023-04-18 15:54:23 +0300

    after this step i can break some estuff wish me luck

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,9 +7,30 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen
     mx-auto">
-      <div className={`${styles.paddingX}`}>
+      <div className={`${styles.paddingX} absolute
+      inset-0 top-[120px] max-w-7xl mx-auto flex
+      flex-row items-start gap-5`}>
+        <div className="flex flex-col
+         justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full 
+          bg-[#046380]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
 
+        <div>
+          <hi className={`${styles.heroHeadText}
+          text-white`} >Hi, I'm <span
+           className="text-[#047e80]">Salih</span> </hi>
+           <p className={`${styles.heroSubText}mt-2
+           text-white-100`}> I analysis data, visualize <br
+           className="sm:block hidden"/> 
+           it and turn them into meaningful information 
+           with appropriate dashboards!
+           </p>
+        </div>
       </div>
+
+      <ComputersCanvas />
     </section>
   )
 }
